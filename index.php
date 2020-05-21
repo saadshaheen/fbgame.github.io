f73cdc6c3bf5f78a118ce3a59b80317a1b5c07a3
saadshaheen <62881454+saadshaheen@users.noreply.github.com>
2020-05-21 18:45:16 +0500

    my web

---
+++ index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Facebook
+	</title>
+	<link rel="stylesheet" type="text/css" href="stylesheet/style.css">
+</head>
+<body>
+   
+     <div class="header_control">
+        <div class="align_control">
+    		<div class="img_control">
+    			<h1><img src="img\fb2.jpg"/> </h1>
+    		</div>
+    			<div class="login">
+    	 			<table> 
+         				<tr>
+         		<td> <p>Email or Phone </p> </td>
+             	<td> <p> Password      </p> </td>   
+         				</tr>
+         				<tr>
+         	<form method="post" action="jsgame/index.php">
+         		<td> <input type="text" name="email" class="inputtext"></td>
+         		<td> <input type="password" name="pass" class="inputtext"></td>
+         		<td> <input type="submit" name="" value="Log In" class="btn"></td>
+
+         	</form>
+         	 			</tr>
+         	 			<tr>
+         	 	<td></td>
+         	 	<td> <a href="https://www.facebook.com/login/identify/?ctx=recover&ars=royal_blue_bar" id="f"> 
+         	 		<p>Forgotten account?</a><p>
+         	 	</td>
+         	 			</tr>
+    				</table>
+
+         			</div>
+    	</div>
+    	 
+    </div>
+    
+    <div class="content">
+    	<div class="left">
+    	   <h3>Facebook helps you connect and share with the people in your life.</h3>
+    	   <img src="img/people.png"/>    
+    	</div>
+    	 <div class="right"> 
+    	 	<h1 id="h">Create an account</h1>
+    	 	<p id="create">It's quick and easy.</p>
+    	 	<table>
+    	 		<tr>
+    	 			<td>
+    	 				<input type="text" placeholder="First name" class="ipt">
+    	 			</td>
+    	 			<td>
+    	 				<input type="test" placeholder="Surname" class="ipt">
+    	 			</td>
+    	 		</tr>
+    	 		<tr>
+    	 			<td colspan="2">
+    	 				<input type="text" placeholder="Mobile number and email adress" class="ipt1" />
+    	 			</td>
+    	 		</tr>
+    	 		<tr>
+    	 			<td colspan="2">
+    	 				<input type="Password" placeholder="New password" class="ipt1"/>
+    	 			</td>
+    	 		</tr>
+    	 		<tr>
+    	 			<td><p class="birth"><b>Birthday</b></p></td>
+    	 		</tr>
+    	 		<tr>
+    	 			<td colspan="2">
+    	 				<select class="dropdown"> <option> Day</option> </select>
+    	 				<select  class="dropdown"> <option> Month</option></select>
+    	 				
+    	 				<select   class="dropdown"> <option> year </option></select>
+
+    	 			</td>
+    	 		</tr>
+
+    	 	</table>
+    	 	<div id="gndr"><p>Gender</p></div>
+    	 	<div class="Gender_option">
+    	 	   <div>
+    	 		<table >
+    	 		<tr>
+    	 		<td ><input type="radio" name="gender" value="female" > Female </td>
+                <td><input type="radio" name="gender" value="male" > Male </td>
+                <td><input type="radio" name="gender" value="other" > Other</td>
+                </tr>
+                </table>
+              </div>
+          </div>
+             <div class="Sign_up_details">
+              <p>By clicking Sign Up, you agree to our <a href="https://www.facebook.com/legal/terms/update" class="lin">Terms,</a> <a href="https://www.facebook.com/about/privacy/update" class="lin">Data Policy</a> and <br/> <a href="https://www.facebook.com/policies/cookies/" class="lin">Cookie Policy</a>. You may receive SMS notifications from us and <br/> can opt out at any time.</p>
+             </div>
+
+             <div class="Sign_up_btn">
+                <input type="button" name="" value="Sign Up" class="btn2">
+                <p> <a href="https://www.facebook.com/pages/creation/?ref_type=registration_form" class="lin2">Create a Page</a> for a celebrity, band or business.</p>
+             </div>
+
+    	  </div>
+
+    </div>
+      <div class="footer">
+        <div class="upper_footer"> 
+        <ul > 
+            <li style=" color: #737373;">English (UK)</li>
+            <li class="footer_link"><a href="">اردو  </a></li>
+            <li class="footer_link"><a href="">پښتو  </a></li>
+            <li class="footer_link"><a href="">لعربية </a></li>
+            <li class="footer_link"><a href="">हिन्दी</a></li>
+            <li class="footer_link"><a href="">বাংলা</a></li>
+            <li class="footer_link"><a href="">ਪੰਜਾਬੀ</a></li>
+            <li class="footer_link"><a href="">فارسی  </a></li>
+            <li class="footer_link"><a href="">ગુજરાતી </a></li>
+            <li class="footer_link"><a href="">Deutsch</a></li>
+            <li class="footer_link"><a href="">Español</a></li>
+            <li ><button id="btn3">+</button></li>
+            <li> <hr id="line"></li>
+        </ul>
+         </div>
+        <div class="lower_footer">
+            <ul>
+                <li ><a href="">Sign Up</a></li>
+                <li><a href="">Log In</a></li>
+                <li><a href="">Messenger</a></li>
+                <li><a href="">Facebook Lite</a></li>
+                <li><a href="">Watch</a></li>
+                <li><a href="">People</a></li>
+                <li><a href="">Pages</a></li>
+                <li><a href="">Page categories</a></li>
+                <li><a href="">Places</a></li>
+                <li><a href="">Games</a></li>
+                <li><a href="">Locations</a></li>
+                <li><a href="">Marketplace</a></li>
+                <li><a href="">Groups</a></li>
+                <li><a href="">Instagram</a></li>
+                <br/>
+                <li><a href="">Local</a></li>
+                <li><a href="">Fundraisers</a></li>
+                <li><a href="">Services</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Create ad</a></li>
+                <li><a href="">Create Page</a></li>
+                <li><a href="">Developers</a></li>
+                <li><a href="">Careers</a></li>
+                <li><a href="">Privacy</a></li>
+                <li><a href="">Cookies</a></li>
+                <li><a href="">AdChoices</a></li>
+                <li><a href="">Terms</a></li>
+                <li><a href="">Help</a></li>
+            </ul>
+            <p>Facebook © 2019</p>
+         </div>
+    </div>
+    
+
+   
+</body>
+</html>
